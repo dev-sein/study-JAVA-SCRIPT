ba32b71660b4f6c073fd9866aaac984e7befe881
dev-sein <seinee114@gmail.com>
2023-02-10 17:14:03 +0900

    Create JAVASCRIPT EventTask

---
+++ workspace/day05/task02/task02.js
@@ -0,0 +1,27 @@
+/* 
+    task02.html
+*/
+
+globalThis.temp;
+const button = document.querySelector("button");
+const input = document.querySelector("#numbertohangle");
+const output = document.querySelector(".output");
+let hangle = "공일이삼사오육칠팔구".split("");
+
+button.addEventListener("click", function () {
+    var arinput = input.value.split("");
+    var result='';
+    for (let i = 0; i < arinput.length; i++) {
+        if(hangle[arinput[i]]){
+            result += hangle[arinput[i]];
+        }else if(arinput[i]=="."){
+            result+="점";
+        }
+        else{
+            result="숫자만 입력해주세요";
+            break;
+        }
+    }
+    output.innerHTML = result;
+}
+);
